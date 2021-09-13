ff433667d164e47c9a79442a4f5c91709ef20b40
md.asif.8393 <md.asif.8393@gmail.com>
2021-09-13 14:37:48 +0600

    complete all things

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ const showProducts = (products) => {
       <small class="text-warning">Rating: ${product.rating.rate} || Total rated: ${product.rating.count} person</small>
       <h2 class="text-info">Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-info">Details</button></div>
+      <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
 
     document.getElementById("all-products").appendChild(div);
